c240fff69703e2a129041576926c24aedea1c247
sheral-shah <73929692+sheral-shah@users.noreply.github.com>
2020-11-04 22:56:32 +0530

    Update Processing.js

--- Processing.js
+++ Processing.js
@@ -1,8 +1,39 @@
-*Java Script
-*Creating a canvas
+//Java Script
+//Creating a canvas
 size(200,200);
-*Creating Shapes
-point(50,80);
-line(20,30,80,30);
-rectangle(20,30,40,60); *OR
+//Creating Shapes
+point(50,80); //50,80 are to coordinates for the canvas
+line(20,30,80,30); //20,20 are the coordinates for the beggining point and 80,30 for the ending point
+rect(20,30,40,60); //20,30 are the cooordinates and 40,60 are the width and height of rectangle
+//Other ways of creating a rectangle are:
+rectMode (CENTER);
+rect(20,30,40,60) //In the first code 20,30 was the left point of the rectangle. Here the rectangles will be created around the centre point which will be at 20,30
+rectMODE (CORNERS);
+rect (40,40,60,60) //Specify coordinates of the upper left and lower right corners
+ellipse(20,20,40,40) //20,20 are coordinates 40,40 width and height
+ellipseMode (CENTRE);
+ellipse(20,20,40,40) //ellipse of size 40,40 will be centred around the coordinate 20,20
+ellipseMode (CORNER);
+ellipse (20,20,40,40) //20,20 are the upper left cordinates
+ellipseMode (CORNERS);
+ellipse (20,20,40,40) //20,20 and 40,40 are the upper left and lower right cooordinates
+
+
+//A DESIGN
+size(300,300);
+ellipse(150,150,30,30);
+ellipse(130,120,40,40);
+ellipse(170,120,40,40);
+ellipse(185,158,40,40);
+ellipse(152,185,40,40);
+ellipse(115,158,40,40);
+rectMode (CORNER);
+rect (0,0,50,45)
+rectMode (CORNER);
+rect (248,253,50,45)
+//
+
+
+
+
 
